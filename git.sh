064ae75fb30f9a57ec9db2747d9eb450764b6dc8
Mokar <tomsknets@yandex.ru>
2017-11-01 13:32:51 +0600

    Original

---
+++ git.sh
@@ -0,0 +1,6 @@
+#echo "# SignalLogic" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+git remote add origin https://github.com/Mokarski/SignalLogic.git
+git push -u origin master
