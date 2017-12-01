4593c02675b109a6760ff6f28221ef47aabb6f51
Mokar <tomsknets@yandex.ru>
2017-12-01 15:21:35 +0600

    Modification signals.cfg and upload.sh

--- copy_to_pcproc.sh
+++ copy_to_pcproc.sh
@@ -1,4 +1,8 @@
+scp client_wago root@192.168.1.121:/mnt/software/bin/
+scp client_logic root@192.168.1.121:/mnt/software/bin/
 scp client_virtual root@192.168.1.121:/mnt/software/bin/
 scp client_modbus root@192.168.1.121:/mnt/software/bin/
 scp signalrouter root@192.168.1.121:/mnt/software/bin/
 scp signals.cfg root@192.168.1.121:/mnt/software/bin/
+scp libmodbus.so.5.0.5 root@192.168.1.121:/mnt/software/bin/
+scp start-all root@192.168.1.121:/mnt/software/bin/
